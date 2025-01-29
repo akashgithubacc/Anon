a55f23595d2f7c086298d58f95b11d77779b469a
akashgithubacc <rohansingh25december@gmail.com>
2025-01-29 15:18:20 +0530

    Fixed Type in Message card

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "standalone",
-	experimental: {
-		appDir: true,
-	},
-	api: {
-		bodyParser: false,
-		externalResolver: true,
-	},
 };
 
 module.exports = nextConfig;
